c2b06e9852a0442e4dd7dd78952a8f33a1a40081
mohamedis8890 <mohamed.is8890@gmail.com>
2021-11-19 22:00:30 +0200

    Persisted data to Json Server

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddTask from "./components/AddTask";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    { id: 1, text: "task 1", day: "Feb 5th at 2:30pm", reminder: true },
-    { id: 2, text: "task 2", day: "Feb 5th at 2:30pm", reminder: false },
-    { id: 3, text: "task 3", day: "Feb 5th at 2:30pm", reminder: true },
-  ]);
-
+  const [tasks, setTasks] = useState([]);
   const [formVisibility, setFormVisibility] = useState(false);
 
-  const onDelete = (id) => {
+  const baseURL = "http://localhost:5000/tasks";
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const data = await fetchTasks();
+      setTasks(data);
+    };
+
+    getTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    const res = await fetch(baseURL);
+    const data = await res.json();
+    return data;
+  };
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`${baseURL}/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
+  const onDelete = async (id) => {
+    await fetch(`${baseURL}/${id}`, { method: "DELETE" });
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const curTask = await fetchTask(id);
+    const updTask = { ...curTask, reminder: !curTask.reminder };
+    await fetch(`${baseURL}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+
     setTasks(
       tasks.map((task) =>
         task.id === id ? { ...task, reminder: !task.reminder } : task
@@ -24,11 +53,17 @@ function App() {
     );
   };
 
-  const submitTask = (newTask) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
+  const submitTask = async (newTask) => {
+    const res = await fetch(baseURL, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(newTask),
+    });
 
-    newTask = { id, ...newTask };
-    setTasks([...tasks, newTask]);
+    const data = await res.json();
+    setTasks([...tasks, data]);
   };
 
   return (
